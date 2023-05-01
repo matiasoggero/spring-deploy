6b552727d396f0094354d2ad8522c4b5f744d544
Matias Oggero <matiassoggero@gmail.com>
2023-05-01 13:24:20 -0300

    spring heroku app config

---
+++ src/main/java/com/example/springdeploy/TestController.java
@@ -0,0 +1,36 @@
+package com.example.springdeploy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${app.message}")
+    String message;
+
+    @GetMapping("/hola")
+    public String hola(){
+        return "hola mundo, saluda matias";
+    }
+
+    @GetMapping("/boostramp")
+    public String boostramp(){
+        return """
+                <!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Bootstrap demo</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h1>Hello, world!</h1>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+                  </body>
+                </html>
+                """;
+    }
+}
